99edc3fd82ed0f085668106a1ea32426711d879e
RadiantCoder3 <166297418+RadiantCoder3@users.noreply.github.com>
2024-06-15 21:16:45 -0400

    Update rustserversetup.py

--- rustserversetup.py
+++ rustserversetup.py
@@ -2,37 +2,30 @@ import os
 import subprocess
 import requests
 
-def install_steamcmd():
+def install_steamcmd(username):
     print("Checking for SteamCMD installation...")
-    if not os.path.exists('C:\\steamcmd\\steamcmd.exe'):
+    steamcmd_path = f'/home/{username}/steamcmd/steamcmd.sh'
+    if not os.path.exists(steamcmd_path):
         print("SteamCMD not found, installing...")
-        steamcmd_url = 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip'
-        os.makedirs('C:\\steamcmd', exist_ok=True)
-        os.chdir('C:\\steamcmd')
+        steamcmd_url = 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz'
+        os.makedirs(f'/home/{username}/steamcmd', exist_ok=True)
+        os.chdir(f'/home/{username}/steamcmd')
         response = requests.get(steamcmd_url)
-        with open('steamcmd.zip', 'wb') as f:
+        with open('steamcmd_linux.tar.gz', 'wb') as f:
             f.write(response.content)
-        subprocess.run(['powershell', '-command', "Expand-Archive", '-Path', 'steamcmd.zip', '-DestinationPath', '.'], check=True)
-        os.chdir('..')
+        subprocess.run(['tar', '-xvzf', 'steamcmd_linux.tar.gz'], check=True)
     else:
         print("SteamCMD is already installed.")
 
-def install_rust_server():
+def install_rust_server(username):
     print("Installing Rust Server...")
-    subprocess.run(['C:\\steamcmd\\steamcmd.exe', '+login', 'anonymous', '+force_install_dir', 'C:\\rust_server', '+app_update', '258550', 'validate', '+quit'], check=True)
-
-def create_start_script():
-    print("Creating server start script...")
-    with open('start_rust_server.bat', 'w') as f:
-        f.write('''@echo off
-cd C:\\rust_server
-RustDedicated -batchmode +server.ip 0.0.0.0 +server.port 28015 +server.tickrate 30 +server.hostname "Your Server Name" +server.identity "my_server" +server.maxplayers 50 +server.worldsize 3000 +server.saveinterval 300 +rcon.ip 0.0.0.0 +rcon.port 28016 +rcon.password "YourRconPassword" -nographics''')
+    subprocess.run([f'/home/{username}/steamcmd/steamcmd.sh', '+login', 'anonymous', '+force_install_dir', f'/home/{username}/rust_server', '+app_update', '258550', 'validate', '+quit'], check=True)
 
 def main():
-    install_steamcmd()
-    install_rust_server()
-    create_start_script()
-    print("Rust server setup is complete. Start your server by running 'start_rust_server.bat'.")
+    username = input("Enter your Linux username: ")
+    install_steamcmd(username)
+    install_rust_server(username)
+    print(f"Rust server setup is complete! Runing command './RustDedicated' in the '/home/{username}/rust_server' folder to start the server.")
 
 if __name__ == "__main__":
     main()
